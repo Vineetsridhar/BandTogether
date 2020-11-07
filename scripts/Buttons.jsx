0e862b2eae0e76197bdce4e1e44ab186bcb1e861
Vineet Sridhar <vineets1600@gmail.com>
2020-11-07 17:24:50 -0500

    Piano works as expected

--- scripts/Buttons.jsx
+++ scripts/Buttons.jsx
@@ -1,12 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { CustomButton } from "./CustomButton";
 import { Stack } from "office-ui-fabric-react";
 import { Piano, KeyboardShortcuts, MidiNumbers } from "react-piano";
+import { socket } from "./Socket";
 
 const TONES = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
 const OCTAVE_NUMBERS = [4, 5];
 
 export const Buttons = ({ playSound, instrument }) => {
+  const sendToServer = (event, note) => {
+    socket.emit(event, { note, instrument });
+  };
   const getAllButton = () => {
     return OCTAVE_NUMBERS.map((OCTAVE) =>
       TONES.map((TONE) => (
@@ -19,8 +23,8 @@ export const Buttons = ({ playSound, instrument }) => {
     ).flat();
   };
 
-  const firstNote = MidiNumbers.fromNote("c3");
-  const lastNote = MidiNumbers.fromNote("f5");
+  const firstNote = MidiNumbers.fromNote("c4");
+  const lastNote = MidiNumbers.fromNote("c6");
   const keyboardShortcuts = KeyboardShortcuts.create({
     firstNote: firstNote,
     lastNote: lastNote,
@@ -32,18 +36,19 @@ export const Buttons = ({ playSound, instrument }) => {
       <Piano
         noteRange={{ first: firstNote, last: lastNote }}
         playNote={(midiNumber) => {
-          // Play a given note - see notes below
+          // console.log(midiNumber);
         }}
-        stopNote={(midiNumber) => {
+        stopNote={(midiNumber, n) => {
           // Stop playing a given note - see notes below
         }}
         width={1000}
         keyboardShortcuts={keyboardShortcuts}
-        onPlayNoteInput={(blah) => {
-          console.log(blah);
+        onPlayNoteInput={(note) => {
+          playSound(note);
+          sendToServer("key_down", note);
         }}
         onStopNoteInput={(blah) => {
-          console.log(blah);
+          // console.log("stop", blah);
         }}
       />
       <Stack horizontal wrap horizontalAlign="space-between" align="center">
