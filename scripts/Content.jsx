302124e06aa5db87c06d27a034c856a4db6ec59c
Vineet Sridhar <vineets1600@gmail.com>
2020-11-07 15:35:34 -0500

    Added piano audio

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,18 +1,39 @@
 
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { socket } from './Socket';
+import AudioPlayer from "./AudioPlayer";
+
+const audioPlayer = AudioPlayer();
+
+const TONES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+const OCTAVE_NUMBERS = [1, 2, 3, 4, 5, 6, 7];
 
 export function Content() {
     useEffect(() => {
-        socket.emit("key_up", {"note": 'a', "instrument": "instrument"})
-        socket.on("key_up", (data) => {
-            console.log(data)
+        audioPlayer.setInstrument("acoustic_grand_piano");
+    }, []);
+
+
+    const onClick = (note) => {
+        audioPlayer.playNote(note);
+    }
+
+    const getAllButton = () => {
+        let buttons = [];
+        TONES.forEach(TONE => {
+            OCTAVE_NUMBERS.forEach(OCTAVE => {
+                const note = TONE+OCTAVE
+                buttons.push(
+                    <button onClick={()=> {onClick(note)}}>{note}</button>
+                )
+            })
         })
-    }, [])
+        return buttons;
+    }
 
     return (
         <div>
-            <button>Send</button>
+            {getAllButton()}
         </div>
     );
 }
