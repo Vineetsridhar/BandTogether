e757e09a5a7f70c61d6908fa27cebb9ed10bad8d
Vineet Sridhar <vineets1600@gmail.com>
2020-11-07 14:59:35 -0500

    Some refactoring and tonejs

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,18 +1,17 @@
 
-import * as React from 'react';
-
+import React, {useEffect} from 'react';
 import { socket } from './Socket';
 
 export function Content() {
-    React.useEffect(() => {
+    useEffect(() => {
         socket.emit("key_up", {"note": 'a', "instrument": "instrument"})
         socket.on("key_up", (data) => {
             console.log(data)
         })
     }, [])
+
     return (
         <div>
-            <h1>Grocery List</h1>
             <button>Send</button>
         </div>
     );
