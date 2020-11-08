127c4fc73747dd5e86aed2d1b2ebb5806479a493
abakirh <abakirhanna@gmail.com>
2020-11-08 04:32:31 -0500

    scoring system

--- scripts/SheetMusic.jsx
+++ scripts/SheetMusic.jsx
@@ -10,29 +10,31 @@ import {
   Stack,
 } from "office-ui-fabric-react";
 import { Animated } from "react-animated-css";
+import { Piano } from "react-piano";
 
 let music = [];
 
 export function SheetMusic() {
   const [upcomingNotes, updateUpcomingNotes] = useState([
-    { note: "Piano A", delay: "0s" },
+    { note: "Piano H", delay: "0s" },
     { note: "Piano G", delay: "5s" },
   ]);
-  music.push(upcomingNotes);
-  console.log(music);
+
   useEffect(() => {
     socket.on("upcoming_note", (data) => {
       updateUpcomingNotes([]);
       setTimeout(() => {
         updateUpcomingNotes(data);
-        console.log(111);
+        
       }, 100);
     });
     upcomingNotes.forEach((note) => {
       console.log(note);
       setTimeout(()=>{
-        console.log(note);
+        console.log(music);
+        // music.push(note);
       }, note.delay.substr(0, note.delay.length-1));
+      
     });
     
   }, []);
@@ -44,6 +46,7 @@ export function SheetMusic() {
         style={{ border: "4px solid #0063B1", height: "200px", width: "50vw" }}
       >
         {upcomingNotes.map((note) => (
+          music.push(note),
           <div style={{ position: "absolute", width: "50vw" }}>
             <p
               className="slideOutDown"
@@ -53,6 +56,7 @@ export function SheetMusic() {
             </p>
           </div>
         ))}
+        
         <div
           style={{
             position: "absolute",
@@ -74,21 +78,38 @@ export function SheetMusic() {
           </p>
         </div>
       </div>
+      
+      {
+        setTimeout(() => {
+          music.pop();
+        }, 10000)
+      }
     </>
   );
 }
-
+let score = 0;
 export function checkNote(note) {
-  // checkNote((note) => {
-  //   // socket.on("upcoming_note", (data) => {
-  //   //   updateUpcomingNotes([]);
-  //   //   setTimeout(() => {
-  //   //     updateUpcomingNotes(data);
-  //   //   }, 100);
-  //   // });
-  //   console.log(note);
-  // }, []);
-  console.log(note);
+  let painoMap = new Map([
+    ['A', 60],
+    ['W', 61],
+    ['S', 62],
+    ['E', 63],
+    ['D', 64],
+    ['F', 65],
+    ['T', 66],
+    ['G', 67],
+    ['Y', 68],
+    ['H', 69],
+    ['U', 70],
+    ['J', 71],
+    ['K', 72]
+  ]);
   console.log(music);
-
+  if(music !== undefined && music.length != 0) {
+    if(painoMap.get(music[0].note.substr(music[0].note.length-1)) == note){
+      score+=5;
+      music.shift();
+    }
+  }
+  console.log(score);
 }
