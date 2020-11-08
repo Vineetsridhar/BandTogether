b63f16448985e646f5eac191e3d42b2efff9ba3c
Sam <student979@live.com>
2020-11-08 04:32:48 -0500

    make stuff look prettier

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -2,30 +2,80 @@ import React, { useEffect, useState } from "react";
 import { socket } from "./Socket";
 import { Buttons } from "./Buttons";
 import AudioPlayer from "./AudioPlayer";
-import { Stack } from "office-ui-fabric-react";
+import { DefaultButton, PrimaryButton, Stack } from "office-ui-fabric-react";
 import { SheetMusic } from "./SheetMusic";
 import Images from "./images";
 import { CustomPiano } from "./CustomPiano";
+import { adjectives, animals } from "./names";
 
 const audioPlayers = {};
 
-const instruments = [
-  "accordion",
-  "acoustic_grand_piano",
-  "acoustic_guitar_nylon",
-  "steel_drums",
-  "alto_sax",
-  "synth_drum",
-];
-
 export function Content() {
   const [instrument, setInstrument] = useState("acoustic_grand_piano");
-  const [name, setName] = useState("Random");
+  const [name, setName] = useState("Name");
   const [users, setUsers] = useState({});
+  const playSound = (note, ins) => {
+    ins = ins ? ins : instrument;
+
+    if (!audioPlayers.hasOwnProperty(ins)) {
+      audioPlayers[ins] = AudioPlayer();
+      audioPlayers[ins].setInstrument(instrument);
+    }
+    audioPlayers[ins].playNote(note);
+  };
+
+  const instruments = {
+    accordion: {
+      name: "Accordion",
+      ui: <Buttons playSound={playSound} instrument={instrument} name={name} />,
+      image:
+        "https://e7.pngegg.com/pngimages/466/517/png-clipart-cartoon-red-accordion-accordion-cartoon-musical-instrument-thumbnail.png",
+    },
+    acoustic_grand_piano: {
+      name: "Piano",
+      ui: (
+        <CustomPiano
+          playSound={playSound}
+          instrument={instrument}
+          name={name}
+        />
+      ),
+      image:
+        "https://p7.hiclipart.com/preview/214/760/914/beautiful-jazz-instruments.jpg",
+    },
+    acoustic_guitar_nylon: {
+      name: "Guitar",
+      ui: <Buttons playSound={playSound} instrument={instrument} name={name} />,
+      image:
+        "https://cf.ltkcdn.net/guitar/images/std/201173-350x350-Free-Guitar-Clip-Art-1.jpg",
+    },
+    steel_drums: {
+      name: "Steel Drums",
+      ui: <Buttons playSound={playSound} instrument={instrument} name={name} />,
+      image:
+        "https://www.clker.com/cliparts/5/c/d/3/15163052441523204714clipart-steel-drum.med.png",
+    },
+    alto_sax: {
+      name: "Alto Sax",
+      ui: <Buttons playSound={playSound} instrument={instrument} name={name} />,
+      image:
+        "https://www.pinclipart.com/picdir/middle/420-4201427_sax-clipart.png",
+    },
+    synth_drum: {
+      name: "Synth Drums",
+      ui: <Buttons playSound={playSound} instrument={instrument} name={name} />,
+      image:
+        "https://i.pinimg.com/564x/32/96/56/329656ec2dc1166cbc8dd92e421665c2.jpg",
+    },
+  };
 
   useEffect(() => {
     audioPlayers[instrument] = AudioPlayer();
     audioPlayers[instrument].setInstrument(instrument);
+    const randomAdjective =
+      adjectives[Math.floor(Math.random() * adjectives.length)];
+    const randomAnimal = animals[Math.floor(Math.random() * animals.length)];
+    setName(`${randomAdjective} ${randomAnimal}`);
 
     socket.on("key_up", (data) => {
       console.log(data);
@@ -43,37 +93,6 @@ export function Content() {
     });
   }, []);
 
-  const playSound = (note, ins) => {
-    ins = ins ? ins : instrument;
-
-    if (!audioPlayers.hasOwnProperty(ins)) {
-      audioPlayers[ins] = AudioPlayer();
-      audioPlayers[ins].setInstrument(instrument);
-    }
-    audioPlayers[ins].playNote(note);
-  };
-
-  const instrumentUIs = {
-    accordion: (
-      <Buttons playSound={playSound} instrument={instrument} name={name} />
-    ),
-    acoustic_grand_piano: (
-      <CustomPiano playSound={playSound} instrument={instrument} name={name} />
-    ),
-    acoustic_guitar_nylon: (
-      <Buttons playSound={playSound} instrument={instrument} name={name} />
-    ),
-    steel_drums: (
-      <Buttons playSound={playSound} instrument={instrument} name={name} />
-    ),
-    alto_sax: (
-      <Buttons playSound={playSound} instrument={instrument} name={name} />
-    ),
-    synth_drum: (
-      <Buttons playSound={playSound} instrument={instrument} name={name} />
-    ),
-  };
-
   const stackItemStyles = { width: "100%", maxWidth: "none" };
   return (
     <Stack horizontalAlign="space-between" align="center">
@@ -84,13 +103,22 @@ export function Content() {
         <h3 align="center">Play some music together!</h3>
       </Stack.Item>
       <Stack.Item align="center" styles={stackItemStyles}>
-        <input
-          placeholder="Your Name"
-          value={name}
-          onChange={(e) => {
-            setName(e.target.value);
-          }}
-        />
+        <Stack
+          horizontal
+          wrap
+          align="center"
+          horizontalAlign="center"
+          tokens={{ childrenGap: 10, padding: 10 }}
+        >
+          <p>Enter your name:</p>
+          <input
+            placeholder="Your Name"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </Stack>
       </Stack.Item>
       <Stack.Item align="center" styles={stackItemStyles}>
         {Object.values(users).map((user) => (
@@ -100,21 +128,43 @@ export function Content() {
         ))}
       </Stack.Item>
       <Stack.Item align="center" styles={stackItemStyles}>
-        <div className="imageContainer">
-          {instruments.map((i) => (
-            <span style={{ flexDirection: "column", display: "flex", flex: 1 }}>
-              <img
-                className={"image " + (i == instrument ? "selected" : "")}
-                src={Images[i]}
-                onClick={() => {
-                  setInstrument(i);
-                }}
-                style={{ cursor: "pointer" }}
-              />
-              <p style={{ textAlign: "center" }}>{i}</p>
-            </span>
+        <Stack
+          horizontal
+          wrap
+          align="center"
+          horizontalAlign="center"
+          tokens={{ childrenGap: 10, padding: 10 }}
+        >
+          {Object.keys(instruments).map((i) => (
+            <div style={{ maxWidth: "100px" }}>
+              <Stack>
+                <img
+                  className={"image " + (i == instrument ? "selected" : "")}
+                  src={instruments[i].image}
+                  onClick={() => {
+                    setInstrument(i);
+                  }}
+                  style={{ cursor: "pointer" }}
+                />
+                <p style={{ textAlign: "center" }}>{instruments[i].name}</p>
+                {i == instrument ? (
+                  <PrimaryButton styles={{ maxWidth: "100px" }}>
+                    {" Selected "}
+                  </PrimaryButton>
+                ) : (
+                  <DefaultButton
+                    onClick={() => {
+                      setInstrument(i);
+                    }}
+                    styles={{ maxWidth: "100px" }}
+                  >
+                    Play!
+                  </DefaultButton>
+                )}
+              </Stack>
+            </div>
           ))}
-        </div>
+        </Stack>
       </Stack.Item>
       <Stack.Item align="center" styles={stackItemStyles}>
         <div style={{ width: "50vw" }}>
@@ -122,7 +172,7 @@ export function Content() {
         </div>
       </Stack.Item>
       <Stack.Item align="center" styles={stackItemStyles}>
-        <div style={{ width: "50vw" }}>{instrumentUIs[instrument]}</div>
+        <div style={{ width: "50vw" }}>{instruments[instrument].ui}</div>
       </Stack.Item>
     </Stack>
   );
